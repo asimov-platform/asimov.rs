7c933080f143e4c2029d3c07743ba8ce836d4bbc
Arto Bendiken <arto@asimov.so>
2024-07-24 22:15:16 +0900

    Bump the Protoflow dependency.

--- lib/asimov-sdk/src/feature.rs
+++ lib/asimov-sdk/src/feature.rs
@@ -2,6 +2,8 @@
 
 /// The set of features that are enabled in this build of the crate.
 pub static FEATURES: &[&str] = &[
+    #[cfg(feature = "derive")]
+    "derive",
     #[cfg(feature = "tokio")]
     "tokio",
     #[cfg(feature = "tracing")]
