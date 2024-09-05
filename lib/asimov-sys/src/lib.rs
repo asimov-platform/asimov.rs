218e1533c8f0d4fc7ae8dab38f41053755ea203e
Arto Bendiken <arto@asimov.so>
2024-09-05 23:27:32 +0900

    Implement `Display` for all structs.

--- lib/asimov-sys/src/lib.rs
+++ lib/asimov-sys/src/lib.rs
@@ -10,12 +10,21 @@ extern crate num_derive;
 #[cfg(feature = "std")]
 extern crate std;
 
-use core::{ffi::c_int, mem::size_of};
+use core::{
+    ffi::{c_int, CStr},
+    fmt::{Debug, Display},
+    mem::size_of,
+    str::Utf8Error,
+};
 use num_traits::FromPrimitive;
 
 include!("bindgen.rs");
 
 pub const ASI_NULL_HANDLE: AsiInstance = 0;
+
+const _: () = assert!(size_of::<AsiPortState>() == 4, "sizeof(AsiPortState) == 4");
+const _: () = assert!(size_of::<AsiPortType>() == 4, "sizeof(AsiPortType) == 4");
+const _: () = assert!(size_of::<AsiResult>() == 4, "sizeof(AsiResult) == 4");
 
 impl TryFrom<c_int> for AsiPortState {
     type Error = ();
@@ -52,6 +61,136 @@ impl From<std::io::Error> for AsiResult {
     }
 }
 
-const _: () = assert!(size_of::<AsiPortState>() == 4, "sizeof(AsiPortState) == 4");
-const _: () = assert!(size_of::<AsiPortType>() == 4, "sizeof(AsiPortType) == 4");
-const _: () = assert!(size_of::<AsiResult>() == 4, "sizeof(AsiResult) == 4");
+impl AsiBlockDefinition {
+    pub fn name(&self) -> Result<&str, Utf8Error> {
+        unsafe { CStr::from_ptr(self.name.as_ptr()) }.to_str()
+    }
+}
+
+impl AsiBlockParameter {
+    pub fn name(&self) -> Result<&str, Utf8Error> {
+        unsafe { CStr::from_ptr(self.name.as_ptr()) }.to_str()
+    }
+}
+
+impl AsiBlockPort {
+    pub fn name(&self) -> Result<&str, Utf8Error> {
+        unsafe { CStr::from_ptr(self.name.as_ptr()) }.to_str()
+    }
+}
+
+impl AsiBlockUsage {
+    pub fn name(&self) -> Result<&str, Utf8Error> {
+        unsafe { CStr::from_ptr(self.name.as_ptr()) }.to_str()
+    }
+}
+
+impl AsiFlowConnection {}
+
+impl AsiFlowDefinition {
+    pub fn name(&self) -> Result<&str, Utf8Error> {
+        unsafe { CStr::from_ptr(self.name.as_ptr()) }.to_str()
+    }
+}
+
+impl AsiFlowExecution {
+    pub fn name(&self) -> Result<&str, Utf8Error> {
+        unsafe { CStr::from_ptr(self.name.as_ptr()) }.to_str()
+    }
+}
+
+impl AsiModelManifest {
+    pub fn name(&self) -> Result<&str, Utf8Error> {
+        unsafe { CStr::from_ptr(self.name.as_ptr()) }.to_str()
+    }
+}
+
+impl AsiModuleRegistration {
+    pub fn name(&self) -> Result<&str, Utf8Error> {
+        unsafe { CStr::from_ptr(self.name.as_ptr()) }.to_str()
+    }
+}
+
+impl Display for AsiBlockDefinition {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.debug_struct("AsiBlockDefinition")
+            .field("name", &self.name().expect("name should be valid UTF-8"))
+            .field("input_port_count", &self.input_port_count)
+            .field("output_port_count", &self.output_port_count)
+            .field("parameter_count", &self.parameter_count)
+            .finish()
+    }
+}
+
+impl Display for AsiBlockParameter {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.debug_struct("AsiBlockParameter")
+            .field("name", &self.name().expect("name should be valid UTF-8"))
+            .field("default_value", &self.default_value)
+            .finish()
+    }
+}
+
+impl Display for AsiBlockPort {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.debug_struct("AsiBlockPort")
+            .field("name", &self.name().expect("name should be valid UTF-8"))
+            .field("type", &self.type_)
+            .finish()
+    }
+}
+
+impl Display for AsiBlockUsage {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.debug_struct("AsiBlockUsage")
+            .field("name", &self.name().expect("name should be valid UTF-8"))
+            .field("type", &self.type_)
+            .finish()
+    }
+}
+
+impl Display for AsiFlowConnection {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.debug_struct("AsiFlowConnection")
+            .field("source_block", &self.source_block)
+            .field("source_port", &self.source_port)
+            .field("target_block", &self.target_block)
+            .field("target_port", &self.target_port)
+            .finish()
+    }
+}
+
+impl Display for AsiFlowDefinition {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.debug_struct("AsiFlowDefinition")
+            .field("name", &self.name().expect("name should be valid UTF-8"))
+            .field("block_count", &self.block_count)
+            .finish()
+    }
+}
+
+impl Display for AsiFlowExecution {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.debug_struct("AsiFlowExecution")
+            .field("name", &self.name().expect("name should be valid UTF-8"))
+            .field("pid", &self.pid)
+            .finish()
+    }
+}
+
+impl Display for AsiModelManifest {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.debug_struct("AsiModelManifest")
+            .field("name", &self.name().expect("name should be valid UTF-8"))
+            .field("size", &self.size)
+            .finish()
+    }
+}
+
+impl Display for AsiModuleRegistration {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.debug_struct("AsiModuleRegistration")
+            .field("name", &self.name().expect("name should be valid UTF-8"))
+            .finish()
+    }
+}
