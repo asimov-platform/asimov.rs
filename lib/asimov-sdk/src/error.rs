2f9c9f886817ad77a7f1447109c14833f18d649d
Arto Bendiken <arto@asimov.so>
2024-09-07 15:55:38 +0900

    Ensure the error enum covers all cases.

--- lib/asimov-sdk/src/error.rs
+++ lib/asimov-sdk/src/error.rs
@@ -11,6 +11,7 @@ extern crate std;
 #[allow(unused)]
 #[derive(Clone, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub enum Error {
+    TimeoutExpired,
     #[default]
     NotImplemented,
     PreconditionViolated,
@@ -30,6 +31,7 @@ impl Error {
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
+            Self::TimeoutExpired => write!(f, "Timeout expired"),
             Self::NotImplemented => write!(f, "Not implemented"),
             Self::PreconditionViolated => write!(f, "Precondition violated"),
             Self::HostMemoryExhausted => write!(f, "Host memory exhausted"),
@@ -119,13 +121,17 @@ impl TryFrom<c_int> for Error {
 
     fn try_from(code: c_int) -> Result<Self, Self::Error> {
         use asimov_sys::*;
-        Ok(match AsiResult::try_from(code)? {
+        let Ok(result) = AsiResult::try_from(code) else {
+            return Ok(Error::Other(format!("ASI_ERROR_{}", code)));
+        };
+        Ok(match result {
+            AsiResult::ASI_SUCCESS => return Err(()),
+            AsiResult::ASI_TIMEOUT_EXPIRED => Error::TimeoutExpired,
             AsiResult::ASI_ERROR_NOT_IMPLEMENTED => Error::NotImplemented,
             AsiResult::ASI_ERROR_PRECONDITION_VIOLATED => Error::PreconditionViolated,
             AsiResult::ASI_ERROR_HOST_MEMORY_EXHAUSTED => Error::HostMemoryExhausted,
             AsiResult::ASI_ERROR_DEVICE_MEMORY_EXHAUSTED => Error::DeviceMemoryExhausted,
             AsiResult::ASI_ERROR_SIZE_INSUFFICIENT => Error::SizeInsufficient,
-            _ => Error::Other(format!("ASI_ERROR_{}", code)),
         })
     }
 }
@@ -136,6 +142,7 @@ impl TryFrom<Error> for c_int {
     fn try_from(error: Error) -> Result<Self, Self::Error> {
         use asimov_sys::*;
         Ok(match error {
+            Error::TimeoutExpired => AsiResult::ASI_TIMEOUT_EXPIRED,
             Error::NotImplemented => AsiResult::ASI_ERROR_NOT_IMPLEMENTED,
             Error::PreconditionViolated => AsiResult::ASI_ERROR_PRECONDITION_VIOLATED,
             Error::HostMemoryExhausted => AsiResult::ASI_ERROR_HOST_MEMORY_EXHAUSTED,
