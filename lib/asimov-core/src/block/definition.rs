5d3582473e2d9d39dff56dc4207d589dcfe67be9
Arto Bendiken <arto@asimov.so>
2024-10-08 13:29:10 +0400

    Add Serde support for `BlockDefinition`.

--- lib/asimov-core/src/block/definition.rs
+++ lib/asimov-core/src/block/definition.rs
@@ -4,3 +4,14 @@ use crate::flow::BlockDescriptor;
 use core::fmt::Debug;
 
 pub trait BlockDefinition: BlockDescriptor + Debug {}
+
+#[cfg(feature = "serde")]
+impl serde::Serialize for dyn BlockDefinition {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let block_descriptor = self.as_block_descriptor();
+        block_descriptor.serialize(serializer)
+    }
+}
