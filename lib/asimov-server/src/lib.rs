6ae5ffb9ce0ef62045f53771f1a1178320e8d104
Arto Bendiken <arto@asimov.so>
2025-05-08 09:26:53 +0200

    Define initial protocols for the server.

--- lib/asimov-server/src/lib.rs
+++ lib/asimov-server/src/lib.rs
@@ -1,1 +1,10 @@
 // This is free and unencumbered software released into the public domain.
+
+#[cfg(feature = "http")]
+pub mod http;
+
+#[cfg(feature = "mdns")]
+pub mod mdns;
+
+#[cfg(feature = "ssdp")]
+pub mod ssdp;
