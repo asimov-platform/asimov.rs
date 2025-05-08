e7d669e49b24b44e921c5e27aac7ce4b755ba0e5
Arto Bendiken <arto@asimov.so>
2025-05-08 11:03:17 +0200

    Structure the HTTP listener.

--- lib/asimov-server/src/http.rs
+++ lib/asimov-server/src/http.rs
@@ -1,1 +1,41 @@
 // This is free and unencumbered software released into the public domain.
+
+use axum::{response::Json, routing::get, Router};
+use std::io::Error;
+use tokio::{
+    net::{TcpListener, ToSocketAddrs},
+    task::JoinHandle,
+};
+use tokio_util::sync::CancellationToken;
+use tower_http::cors::CorsLayer;
+use tracing::info;
+
+pub async fn start(
+    addr: impl ToSocketAddrs,
+) -> std::io::Result<(JoinHandle<Result<(), Error>>, CancellationToken)> {
+    let app = Router::new()
+        .route("/", get(http_handler))
+        .layer(CorsLayer::permissive());
+
+    let listener = TcpListener::bind(addr).await?;
+
+    info!(
+        "Listening for HTTP requests on {}...",
+        listener.local_addr().unwrap()
+    );
+
+    let token = CancellationToken::new();
+    let token_copy = token.clone();
+
+    let handle = tokio::spawn(async move {
+        axum::serve(listener, app)
+            .with_graceful_shutdown(token_copy.cancelled_owned())
+            .await
+    });
+
+    Ok((handle, token))
+}
+
+async fn http_handler() -> Json<&'static str> {
+    Json("Hello, world!") // TODO
+}
--- lib/asimov-server/src/lib.rs
+++ lib/asimov-server/src/lib.rs
@@ -1,5 +1,8 @@
 // This is free and unencumbered software released into the public domain.
 
+pub use tokio::{net::ToSocketAddrs, task::JoinHandle};
+pub use tokio_util::sync::CancellationToken;
+
 #[cfg(feature = "http")]
 pub mod http;
 
