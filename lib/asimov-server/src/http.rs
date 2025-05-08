2f79d1e566f5878eb812dba7debac9c4bf3927c1
Arto Bendiken <arto@asimov.so>
2025-05-08 12:49:54 +0200

    Export Prometheus metrics.

--- lib/asimov-server/src/http.rs
+++ lib/asimov-server/src/http.rs
@@ -1,14 +1,27 @@
 // This is free and unencumbered software released into the public domain.
 
 use axum::{response::Json, routing::get, Router};
+use axum_prometheus::PrometheusMetricLayer;
+use std::time::Duration;
 use tokio::net::{TcpListener, ToSocketAddrs};
 use tokio_util::sync::CancellationToken;
 use tower_http::cors::CorsLayer;
 use tracing::info;
 
 pub async fn start(addr: impl ToSocketAddrs, cancel: CancellationToken) -> std::io::Result<()> {
+    let (prometheus_layer, metric_handle) = PrometheusMetricLayer::pair();
+
     let app = Router::new()
+        .route("/fast", get(|| async {}))
+        .route(
+            "/slow",
+            get(|| async {
+                tokio::time::sleep(Duration::from_secs(1)).await;
+            }),
+        )
+        .route("/metrics", get(|| async move { metric_handle.render() }))
         .route("/", get(http_handler))
+        .layer(prometheus_layer)
         .layer(CorsLayer::permissive());
 
     let listener = TcpListener::bind(addr).await?;
