4d25f0b86739a5acd7dfc439fff34bdd84e47873
Samuel Sarle <samuel@sarle.com>
2025-05-14 14:58:04 +0300

    Replace reference to draft spec with latest

--- lib/asimov-server/src/http/mcp.rs
+++ lib/asimov-server/src/http/mcp.rs
@@ -5,7 +5,7 @@ use axum::{
     routing::{get, post},
 };
 
-/// See: https://modelcontextprotocol.io/specification/draft/basic/transports#streamable-http
+/// See: https://modelcontextprotocol.io/specification/2025-03-26/basic/transports#streamable-http
 pub fn routes() -> Router {
     Router::new().route("/mcp", post(|| async {}))
 }
