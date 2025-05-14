65d6e6d415c3d1ba8cde36383f6fa726bbb3fffb
Samuel Sarle <samuel@sarle.com>
2025-05-14 15:13:54 +0300

    Add an MCP stub for HTTP. (#4)

---
+++ lib/asimov-server/src/http/mcp.rs
@@ -0,0 +1,11 @@
+// This is free and unencumbered software released into the public domain.
+
+use axum::{
+    Router,
+    routing::{get, post},
+};
+
+/// See: https://modelcontextprotocol.io/specification/2025-03-26/basic/transports#streamable-http
+pub fn routes() -> Router {
+    Router::new().route("/mcp", post(|| async {}))
+}
