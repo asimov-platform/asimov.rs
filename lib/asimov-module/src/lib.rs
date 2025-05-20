e82a8d921ece9d29913e91ecd0cfdb2c66712d60
Arto Bendiken <arto@asimov.so>
2025-05-20 11:59:49 +0200

    Make asimov-module buildable.

--- lib/asimov-module/src/lib.rs
+++ lib/asimov-module/src/lib.rs
@@ -1,3 +1,9 @@
 // This is free and unencumbered software released into the public domain.
 
 #![no_std]
+#![deny(unsafe_code)]
+
+pub use dogma::prelude;
+
+#[cfg(feature = "tracing")]
+pub use tracing;
