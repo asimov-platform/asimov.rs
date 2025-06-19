6e2dfd224f66c7e55317a731c45d0d671d4d8af8
Samuel Sarle <samuel@sarle.com>
2025-06-19 13:45:34 +0300

    Simplify example

--- lib/asimov-module/examples/decode_manifest.rs
+++ lib/asimov-module/examples/decode_manifest.rs
@@ -1,14 +1,12 @@
 // This is free and unencumbered software released into the public domain.
 
 use asimov_module::models::Manifest;
-use std::{error::Error, io::Read};
+use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut input = String::new();
-    let _n = std::io::stdin().read_to_string(&mut input)?;
-    let manifest: Manifest = serde_yml::from_str(&input)?;
-    println!("Debug print:\n\n{manifest:?}");
-    let output = serde_yml::to_string(&manifest)?;
-    println!("\nRe-encoded:\n\n{output}");
+    let manifest = serde_yml::from_reader::<_, Manifest>(std::io::stdin())?;
+    print!("Debug print:\n\n{manifest:?}\n\n");
+    print!("Re-encoded:\n\n");
+    serde_yml::to_writer(std::io::stdout(), &manifest)?;
     Ok(())
 }
