3f1005e5e1891e22152e90ca887d812b41ffe272
Samuel Sarle <samuel@asimov.systems>
2025-07-01 11:30:20 +0300

    Add a helper for initializing `tracing` from modules. (#13)

--- lib/asimov-module/src/lib.rs
+++ lib/asimov-module/src/lib.rs
@@ -21,6 +21,21 @@ pub use tracing;
 #[cfg(feature = "tracing")]
 pub use tracing_subscriber;
 
+#[cfg(all(feature = "std", feature = "cli", feature = "tracing"))]
+pub fn init_tracing_subscriber(
+    options: &clientele::StandardOptions,
+) -> Result<(), alloc::boxed::Box<(dyn core::error::Error + Send + Sync + 'static)>> {
+    extern crate std;
+    tracing_subscriber::fmt()
+        .with_writer(std::io::stderr)
+        .with_max_level(options)
+        .with_level(options.debug || options.verbose > 0)
+        .with_target(options.debug)
+        .with_file(false)
+        .without_time()
+        .try_init()
+}
+
 pub mod models;
 
 pub mod resolve;
