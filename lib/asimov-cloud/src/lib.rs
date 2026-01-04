acc4d5c0d5b7d8f9ece1ac4987e5db1d85a10351
Arto Bendiken <arto@asimov.systems>
2026-01-04 13:12:51 -0800

    Re-export relevant crates from asimov-cloud.

--- lib/asimov-cloud/src/lib.rs
+++ lib/asimov-cloud/src/lib.rs
@@ -1,1 +1,6 @@
 // This is free and unencumbered software released into the public domain.
+
+pub use asimov_credit as credit;
+pub use asimov_id as id;
+pub use asimov_kb as kb;
+pub use asimov_nexus as nexus;
