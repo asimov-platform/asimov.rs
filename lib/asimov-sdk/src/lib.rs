50afa53f62deb2e2a475bd6bff9df91103cd5fd5
Arto Bendiken <arto@asimov.systems>
2026-01-30 15:35:51 -0800

    Export packages from the top-level crate.

--- lib/asimov-sdk/src/lib.rs
+++ lib/asimov-sdk/src/lib.rs
@@ -1,1 +1,19 @@
 // This is free and unencumbered software released into the public domain.
+
+#![no_std]
+#![forbid(unsafe_code)]
+
+pub use asimov_config as config;
+pub use asimov_core as core;
+pub use asimov_credit as credit;
+pub use asimov_env as env;
+pub use asimov_flow as flow;
+pub use asimov_id as id;
+pub use asimov_installer as installer;
+pub use asimov_kb as kb;
+pub use asimov_module as module;
+pub use asimov_patterns as patterns;
+pub use asimov_prompt as prompt;
+pub use asimov_registry as registry;
+pub use asimov_runner as runner;
+pub use asimov_snapshot as snapshot;
